a274de1c2ced77b0b7610e7c72f8a2bd09c90eaf
Alvaro Marinho da Silva Oliveira <162765144+Alvaromarinho75@users.noreply.github.com>
2025-03-02 12:10:16 -0300

    Add files via upload

---
+++ CAS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CAS;
+
+class Program
+{
+    static void Main()
+    {   
+        Expressao x = "x";
+        Expressao expr = new Soma(new Divisao(2, x), 3);
+        Console.WriteLine($"Expressão original: {expr}");
+        Expressao exprD = expr.Derivar((Simbolo)x);
+        Console.WriteLine($"Expressão derivada: {exprD}");
+        Console.WriteLine($"Expressão substituída (x = 5): {expr.Substituir((Simbolo)x, new Numero(5))}");
+    }
+}
