e4c7b6ed11892c7e6fef2ab51702b5e5ba9bedb8
Alvaromarinho75 <alvarocraftbr@gmail.com>
2025-03-23 15:33:11 -0300

    Finalização do preto de analise em LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -16,31 +16,60 @@
 var cotas = CotaParlamentar.LerCotasParlamentares("cota_parlamentar.csv");
 //cotas.OrderByDescending(c => c.ValorLiquido).Take(10).ToArray().Dump();
 
-/*
+
+// 1)
 var partidos = cotas.GroupBy(c => c.Partido)
     .Select(g => new { Partido = g.Key, Total = g.Sum(c => c.ValorLiquido) })
     .OrderByDescending(g => g.Total).Take(10).ToArray().Dump();
 
+// 2)
 var deputados = cotas.GroupBy(c => c.NomeParlamentar)
     .Select(g => new { Nome = g.Key, Total = g.Sum(c => c.ValorLiquido) })
     .OrderByDescending(g => g.Total).Take(5).ToArray().Dump();
 
-var meses = cotas.GroupBy(c => c.Mes)
+
+// 3)
+var meses3 = cotas.GroupBy(c => c.Mes)
     .Select(g => new { Mes = g.Key, Media = Math.Round(g.Average(c => c.ValorLiquido) ?? 0, 2) })
     .OrderBy(g => g.Mes).ToArray().Dump();
 
-
+// 4)
 var alimentacaoGastos = cotas
     .Where(c => c.Descricao.Contains("ALIMENTAÇÃO"))
     .GroupBy(c => c.NomeParlamentar)
     .Select(g => new { Nome = g.Key, Total = g.Sum(c => c.ValorLiquido) })
     .OrderByDescending(g => g.Total).ToArray().Dump();
 
+// 5)
 var fornecedores = cotas.GroupBy(c => c.Fornecedor)
     .Select(g => new { Fornecedor = g.Key, Total = g.Count() })
     .OrderByDescending(g => g.Total).Take(10).ToArray().Dump();
-*/
+
+
+// 6)
 var UFs = cotas.GroupBy(c => c.UF)
     .Select(g => new { UF = g.Key, Total = g.Sum(c => c.ValorLiquido) })
     .OrderByDescending(g => g.Total).ToArray().Dump();
+
+
+// 7)
+var meses7 = cotas.GroupBy(c => c.Mes)
+    .Select(g => new {Mes = g.Key, Documentos = g.Count( c => c.DocumentoId.HasValue) })
+    .OrderBy(g => g.Mes).ToArray().Dump();
+
+// 8)
+var deputadosComDespesasAltas = cotas.GroupBy(c => c.NomeParlamentar)
+    .Select(g => new { Nome = g.Key, Total = g.Sum(c => c.ValorLiquido) })
+    .Where(g => g.Total > 10000)
+    .OrderByDescending(g => g.Total).ToArray().Dump();
+
+// 9)
+var totalPorTipoDespesa = cotas.GroupBy(c => c.Descricao)
+    .Select(g => new { Descricao = g.Key, Total = g.Sum(c => c.ValorLiquido) })
+    .OrderByDescending(g => g.Total).ToArray().Dump();
+
+// 10)
+var totalPorAno = cotas.GroupBy(c => c.Ano)
+    .Select(g => new { Ano = g.Key, Total = g.Sum(c => c.ValorLiquido) })
+    .OrderBy(g => g.Ano).ToArray().Dump();
 
