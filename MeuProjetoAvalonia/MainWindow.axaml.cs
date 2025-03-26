8916b72ecd313507ddc63bb3ebbb48f47cd1d3c8
Alvaromarinho75 <alvarocraftbr@gmail.com>
2025-03-26 16:00:43 -0300

    Avalonia

---
+++ MeuProjetoAvalonia/MainWindow.axaml.cs
@@ -0,0 +1,36 @@
+    using Avalonia.Controls;
+    using Avalonia.Interactivity;
+
+    namespace MeuProjetoAvalonia
+    {
+        public partial class MainWindow : Window
+        {
+            public MainWindow()
+            {
+                // Inicializa a interface a partir do XAML
+                InitializeComponent();
+            }
+
+            // Evento chamado quando o botão é clicado
+            private void BtnClique_Click(object sender, RoutedEventArgs e)
+            {
+                var valor = this.FindControl<TextBox>("txtInput").Text;
+
+                // Exibe uma mensagem alterando o texto do próprio TextBox como exemplo
+                if (int.TryParse(valor, out int numero))
+                {   
+                    // Obtém o valor do RadioButton
+                    var rad = this.FindControl<RadioButton>("radOpcao1").IsChecked;
+
+                    switch (rad)
+                    
+                    this.FindControl<TextBox>("txtResultado").Text = $"Olá, {numero + 2}!";
+                }
+                else
+                {
+                    // Caso o usuário não tenha digitado nada, exibe uma mensagem padrão
+                    this.FindControl<TextBox>("txtInput").Text = "Não há valor!";
+                }
+            }
+        }
+    }
