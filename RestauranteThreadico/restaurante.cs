45c75f6ec268c912cf183b0b581e74adb18d41a1
Alvaromarinho75 <alvarocraftbr@gmail.com>
2025-04-09 16:08:08 -0300

    Implementacao de porcoes e multiplos chefes

--- RestauranteThreadico/restaurante.cs
+++ RestauranteThreadico/restaurante.cs
@@ -2,9 +2,19 @@
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.Text;
+using System.Collections.Generic;
 
 BlockingCollection<(int pedido, int prato)> pedidos = new BlockingCollection<(int pedido, int prato)>();
 object lockConsole = new();
+
+Dictionary<string, int> Estoque = new Dictionary<string, int>
+{
+    { "Arroz", 3 },
+    { "Carne", 2 },
+    { "Macarrao", 4 },
+    { "Molho", 2 }
+};
+
 
 void ConsoleLock(string msg, ConsoleColor color)
 {
@@ -17,6 +27,61 @@ void ConsoleLock(string msg, ConsoleColor color)
     }
 }
 int pedido = 0;
+
+int podePreparar(int prato){
+    int p = Interlocked.Increment(ref pedido);
+
+    switch(prato)
+    {
+        case 1:
+            if(Estoque["Arroz"] <= 0){
+                pedidos.Add((p, 4));
+                return 4;
+
+            } if(Estoque["Carne"] <= 0) {
+                pedidos.Add((p, 5));
+                return 5;
+
+            } else {
+                return 0;
+
+            }
+        case 2:
+            if(Estoque["Macarrao"] <= 0){
+                pedidos.Add((p, 6));
+                return 6;
+
+            } if(Estoque["Molho"] <= 0) {
+                pedidos.Add((p, 7));
+                return 7;
+
+            } else {
+                return 0;
+
+            }
+        case 3:
+            if(Estoque["Arroz"] <= 0){
+                pedidos.Add((p, 4));
+                return 4;
+
+            } if(Estoque["Carne"] <= 0) {
+                pedidos.Add((p, 5));
+                return 5;
+
+            } if(Estoque["Molho"] <= 0) {
+                pedidos.Add((p, 7));
+                return 7;
+
+            } else {
+                return 0;
+
+            }
+        default:
+            return 1;
+    }
+
+}
+
 void Garcom()
 {
     var rnd = new Random();
@@ -32,31 +97,90 @@ void Garcom()
 
         ConsoleLock($"[Garcom {id}] Enviei pedido {p} do prato {prato}!", ConsoleColor.Blue);
         pedidos.Add((p, prato));
-        pedido++;
     }
     pedidos.CompleteAdding();
 }
 
 void Chef()
-{
-    Console.WriteLine("[Chef] Estou pronto!!!");
+{   
+    var id = Thread.CurrentThread.ManagedThreadId;
+    Console.WriteLine($"[Chef {id}] Estou pronto!!!");
     foreach(var item in pedidos.GetConsumingEnumerable())
     {
         var (pedido, prato) = item;
-        ConsoleLock($"[Chef] Iniciando o pedido {pedido} do prato {prato}!", ConsoleColor.Red);
 
-        if(prato is 1 or 2) Thread.Sleep(2000);
-        else Thread.Sleep(3000);
+        if(podePreparar(prato) != 0)
+        {
+            pedido = podePreparar(prato);
+        }
+        ConsoleLock($"[Chef {id}] Iniciando o pedido {pedido} do prato {prato}!", 
+            ConsoleColor.Red);
+        ConsoleLock("[Estoque] " + 
+            $"Arroz: {Estoque["Arroz"]} | " +
+            $"Carne: {Estoque["Carne"]} | " +
+            $"Macarrao: {Estoque["Macarrao"]} | " +
+            $"Molho: {Estoque["Molho"]}", ConsoleColor.Yellow);
 
-        ConsoleLock($"[Chef] Finalizado o pedido {pedido} do prato {prato}!", ConsoleColor.Red);
+
+        switch(prato)
+        {
+            case 1:
+                Thread.Sleep(2000);
+                Estoque["Arroz"]--;
+                Estoque["Carne"]--;
+                break;
+            case 2:
+                Thread.Sleep(2000);
+                Estoque["Macarrao"]--;
+                Estoque["Molho"]--;
+                break;
+            case 3:
+                Thread.Sleep(3000);
+                Estoque["Arroz"]--;
+                Estoque["Carne"]--;
+                Estoque["Molho"]--;
+                break;
+            case 4:
+                ConsoleLock($"[Chef {id}] produzindo arroz", ConsoleColor.Green);
+                Thread.Sleep(2000);
+                Estoque["Arroz"] += 3;
+                ConsoleLock($"[Chef {id}] arroz produzido", ConsoleColor.Green);
+                break;
+            
+            case 5:
+                ConsoleLock($"[Chef {id}] produzindo carne", ConsoleColor.Green);
+                Thread.Sleep(2000);
+                Estoque["Carne"] += 2;
+                ConsoleLock($"[Chef {id}] carne produzida", ConsoleColor.Green);
+                break;
+
+            case 6:
+                ConsoleLock($"[Chef {id}] produzindo macarrao", ConsoleColor.Green);
+                Thread.Sleep(2000);
+                Estoque["Macarrao"] += 4;
+                ConsoleLock($"[Chef {id}] macarrao produzido", ConsoleColor.Green);
+                break;
+
+            case 7:
+                ConsoleLock($"[Chef {id}] produzindo molho", ConsoleColor.Green);
+                Thread.Sleep(2000);
+                Estoque["Molho"] += 2;
+                ConsoleLock($"[Chef {id}] molho produzido", ConsoleColor.Green);
+                break;
+        }
+
+        ConsoleLock($"[Chef {id}] Finalizado o pedido {pedido} do prato {prato}!",
+             ConsoleColor.Red);
+        ConsoleLock("[Estoque] " + 
+            $"Arroz: {Estoque["Arroz"]} | " +
+            $"Carne: {Estoque["Carne"]} | " +
+            $"Macarrao: {Estoque["Macarrao"]} | " +
+            $"Molho: {Estoque["Molho"]}", ConsoleColor.Yellow);
     }
-}
+} 
 
 
-var g = Enumerable .Range(1, 5).Select(i => Task.Run(() => Garcom())).ToList();
-var c = new Task(Chef);
-
-c.Start();
-
-c.Wait();
-Task.WaitAll(g);
+var g = Enumerable .Range(1, 3).Select(i => Task.Run(() => Garcom())).ToList();
+var c = Enumerable.Range(1, 2).Select(i => Task.Run(() => Chef())).ToList();
+Task.WaitAll(c.ToArray());
+Task.WaitAll(g.ToArray());
